0975dc1bb33c9a5e8e1fe2940487aff4ee2d9e55
patelshashank18 <132254266+patelshashank18@users.noreply.github.com>
2026-08-06 10:42:23 +0530

    Update practical9.java

--- practical9.java
+++ practical9.java
@@ -1,31 +1,30 @@
-public class practical9 {
+public class Practical9 {
+
     public static void main(String[] args) {
 
-        // Creating Wrapper class object
-        Integer num1 = Integer.valueOf(50);
-        Integer num2 = Integer.valueOf(80);
+        // Creating Wrapper class objects
+        Integer firstNumber = Integer.valueOf(50);
+        Integer secondNumber = Integer.valueOf(80);
 
-        System.out.println("First Number : " + num1);
-        System.out.println("Second Number: " + num2);
+        System.out.println("First Number : " + firstNumber);
+        System.out.println("Second Number: " + secondNumber);
 
-        // Using equals()
-        if (num1.equals(num2)) {
+        // Using equals() method
+        if (firstNumber.equals(secondNumber)) {
             System.out.println("Both numbers are equal.");
         } else {
             System.out.println("Both numbers are not equal.");
         }
 
-        // Using compareTo()
-        int result = num1.compareTo(num2);
+        // Using compareTo() method
+        int comparisonResult = firstNumber.compareTo(secondNumber);
 
-        if (result == 0) {
-            System.out.println("num1 and num2 are equal.");
-        }
-        //result check less than 0
-        else if (result > 0) {
-            System.out.println("num1 is greater than num2.");
+        if (comparisonResult == 0) {
+            System.out.println("Both numbers are equal.");
+        } else if (comparisonResult > 0) {
+            System.out.println("First number is greater than second number.");
         } else {
-            System.out.println("num1 is smaller than num2.");
+            System.out.println("First number is smaller than second number.");
         }
     }
 }
