0cdcbf436a7bde073044859054ca7d6a9613d69e
Shashank Patel <2002shashank@gmail.com>
2026-08-17 11:53:50 +0530

    Edit Name

--- FST-2/ComparableComparator/NameComparator.java
+++ FST-2/ComparableComparator/NameComparator.java
@@ -2,10 +2,10 @@ package ComparableComparator;
 
 import java.util.Comparator;
 
-// Comparator class to sort students by name
+//** Comparator class to sort students by name */
 class NameComparator implements Comparator<Student> {
 
-    // Compare two student names
+    //** Compare two student names */
     public int compare(Student s1, Student s2) {
         return s1.name.compareTo(s2.name);
     }
--- FST-2/ComparableComparator/Student.java
+++ FST-2/ComparableComparator/Student.java
@@ -1,26 +1,27 @@
 package ComparableComparator;
 
-// Student class implements Comparable
+//** Student class implements Comparable */
 class Student implements Comparable<Student> {
 
     int rollNo;
     String name;
     int marks;
 
-    // Constructor to initialize student details
+    //** Constructor to initialize student details */
     Student(int rollNo, String name, int marks) {
         this.rollNo = rollNo;
         this.name = name;
         this.marks = marks;
     }
 
-    // Comparable: Sort students by roll number
+    //**  Comparable: Sort students by roll number */
     public int compareTo(Student s) {
         return this.rollNo - s.rollNo;
     }
 
-    // Display student details
+    //** Display student details *//
     public String toString() {
         return rollNo + " " + name + " " + marks;
+
     }
 }
