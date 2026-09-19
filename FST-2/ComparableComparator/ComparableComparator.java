985f7ec21caef9e5c435b5edf526b7baf2811c18
Shashank Patel <2002shashank@gmail.com>
2026-09-19 10:36:09 +0530

    edit file because of comment line

--- FST-2/ComparableComparator/ComparableComparator.java
+++ FST-2/ComparableComparator/ComparableComparator.java
@@ -13,9 +13,8 @@ public class ComparableComparator {
 
     public static void main(String[] args) {
 
-        /**
-         * Condition 1: Find index of an array element.
-         */
+        // Condition 1: Find index of an array element.
+
         int[] arr = { 10, 20, 30, 40, 50 };
         int search = 30;
 
@@ -26,9 +25,8 @@ public class ComparableComparator {
             }
         }
 
-        /**
-         * Condition 2: List.
-         */
+        // Condition 2: List.
+
         List<Element> list = new ArrayList<>();
 
         list.add(new Element(3, "Rahul", 70));
@@ -37,9 +35,8 @@ public class ComparableComparator {
 
         System.out.println("List: " + list);
 
-        /**
-         * Condition 3: Set.
-         */
+        // Condition 3: Set.
+
         Set<String> set = new HashSet<>();
 
         set.add("Jigar");
@@ -48,9 +45,8 @@ public class ComparableComparator {
 
         System.out.println("Set: " + set);
 
-        /**
-         * Condition 4: Map.
-         */
+        // Condition 4: Map.
+
         Map<Integer, String> map = new HashMap<>();
 
         map.put(1, "Jigar");
@@ -58,22 +54,20 @@ public class ComparableComparator {
 
         System.out.println("Map: " + map);
 
-        /**
-         * Condition 5: Comparable - Sort by ID.
-         */
+        // Condition 5: Comparable - Sort by ID.
+
         Collections.sort(list);
         System.out.println("By ID: " + list);
 
-        /**
-         * Condition 6: Comparator - Sort by Name.
-         */
+        // Condition 6: Comparator - Sort by Name.
+
         Collections.sort(list, new SortByName());
         System.out.println("By Name: " + list);
 
-        /**
-         * Condition 7: Comparator - Sort by Marks.
-         */
+        // Condition 7: Comparator - Sort by Marks.
+
         Collections.sort(list, new SortByMarks());
         System.out.println("By Marks: " + list);
     }
+
 }
