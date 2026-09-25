9fe7d17db2614720b03ab4523a75ed2005993a38
patelshashank18 <132254266+patelshashank18@users.noreply.github.com>
2026-09-25 17:36:49 +0530

    Add Element class with Comparable sorting by ID

--- FST-2/ComparableComparator/Element.java
+++ FST-2/ComparableComparator/Element.java
@@ -1,7 +1,10 @@
 package ComparableComparator;
 
 /**
- * Represents an element with ID, name and marks.
+ * Represents an element with ID, name, and marks.
+ *
+ * This class implements Comparable to allow
+ * elements to be sorted by ID.
  */
 public class Element implements Comparable<Element> {
 
@@ -11,24 +14,29 @@ public class Element implements Comparable<Element> {
 
     /**
      * Constructor to initialize element details.
+     *
+     * @param id    the element ID
+     * @param name  the element name
+     * @param marks the element marks
      */
     Element(int id, String name, double marks) {
+
+        // Assign values to the instance variables
         this.id = id;
         this.name = name;
         this.marks = marks;
     }
 
-    /**
-     * Comparable: Sort elements by ID.
+    /*
+     * Comparable is used to sort elements by ID.
      */
     @Override
     public int compareTo(Element e) {
         return this.id - e.id;
     }
 
-    /**
-     * Returns element details.
-     */
+    // Returns element details as a String.
+    @Override
     public String toString() {
         return id + " " + name + " " + marks;
     }
