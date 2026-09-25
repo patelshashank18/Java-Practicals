08786a1b39ea1755f0e0f2b7717d667354b94342
patelshashank18 <132254266+patelshashank18@users.noreply.github.com>
2026-09-25 17:38:34 +0530

    Add Element comparator for marks

--- FST-2/ComparableComparator/SortByMarks.java
+++ FST-2/ComparableComparator/SortByMarks.java
@@ -4,13 +4,23 @@ import java.util.Comparator;
 
 /**
  * Comparator: Sort elements by marks.
+ *
+ * This class sorts Element objects in descending
+ * order according to their marks.
  */
 public class SortByMarks implements Comparator<Element> {
 
     /**
      * Compares two elements by marks.
+     *
+     * @param a first element
+     * @param b second element
+     * @return comparison result based on marks
      */
+    @Override
     public int compare(Element a, Element b) {
+
+        // Compare marks in descending order
         return Double.compare(b.marks, a.marks);
     }
 }
