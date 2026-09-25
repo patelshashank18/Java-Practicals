440b62d3c117c407548212f2c42c1f787323e369
patelshashank18 <132254266+patelshashank18@users.noreply.github.com>
2026-09-25 17:27:47 +0530

    Add abstract Animal class with sound and eat methods
    Updated comments for clarity and consistency.

--- FST-2/Abstraction/AnimalAbstract.java
+++ FST-2/Abstraction/AnimalAbstract.java
@@ -2,16 +2,18 @@ package Abstraction;
 
 /**
  * Abstract class representing an animal.
+ *
+ * This class defines common behaviors
+ * that must be implemented by child classes.
  */
 abstract class AnimalAbstract {
 
-    /**
+    /*
      * Defines the sound behavior.
+     * Child classes must provide the implementation.
      */
     abstract void sound();
 
-    /**
-     * Defines the eating behavior.
-     */
+    // Defines the eating behavior.
     abstract void eat();
 }
