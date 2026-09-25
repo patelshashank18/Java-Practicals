178a5b149a0904d155f95cdefe8fbfa7d59abb4a
patelshashank18 <132254266+patelshashank18@users.noreply.github.com>
2026-09-25 17:39:02 +0530

    Add Element comparator for name
    Updated comments to improve clarity and fixed formatting.

--- FST-2/ComparableComparator/SortByName.java
+++ FST-2/ComparableComparator/SortByName.java
@@ -4,13 +4,20 @@ import java.util.Comparator;
 
 /**
  * Comparator: Sort elements by name.
+ *
+ * This class sorts Element objects alphabetically
+ * according to their names.
  */
 public class SortByName implements Comparator<Element> {
 
-    /**
+    /*
      * Compares two elements by name.
+     * The comparison is performed alphabetically.
      */
+    @Override
     public int compare(Element a, Element b) {
+
+        // Compare the names of two elements
         return a.name.compareTo(b.name);
     }
 }
