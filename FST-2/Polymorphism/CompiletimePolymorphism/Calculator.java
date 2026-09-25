fbc0703a9b5e38a98b0fa5e576f73dd4e20f5eb9
patelshashank18 <132254266+patelshashank18@users.noreply.github.com>
2026-09-25 17:58:02 +0530

    Add Calculator with overloaded methods
    Updated class documentation and added method comments.

--- FST-2/Polymorphism/CompiletimePolymorphism/Calculator.java
+++ FST-2/Polymorphism/CompiletimePolymorphism/Calculator.java
@@ -1,12 +1,30 @@
 package Polymorphism.CompiletimePolymorphism;
 
-/** Class for method overloading */
+/**
+ * Class for method overloading.
+ *
+ * This class demonstrates compile-time polymorphism
+ * using multiple add() methods.
+ */
 class Calculator {
 
+    /*
+     * Adds two integer values.
+     */
     int add(int a, int b) {
+
+        // Return the sum of two numbers
         return a + b;
     }
 
+    /**
+     * Adds three integer values.
+     *
+     * @param a first number
+     * @param b second number
+     * @param c third number
+     * @return sum of three numbers
+     */
     int add(int a, int b, int c) {
         return a + b + c;
     }
